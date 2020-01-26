cf0d669a42fbdd5c09a93b2ebfd4bd193348fed1
Mike P <mikhail.pershin@hotmail.com>
2020-01-26 14:31:27 +0000

    add: user page

---
+++ README.md
@@ -0,0 +1,10 @@
+# Rankme web panel
+
+Web panel for rankme CS:GO server plugin.
+
+# Installation
+
+```
+php 7.1+
+composer
+```
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,10 +6,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 use App\Entity\Rankme;
 
 class DefaultController extends AbstractController
 {
+    /**
+      * @Route("/", name="index", methods={"GET"})
+      */
     public function index()
     {
         $users = $this->getDoctrine()
@@ -25,5 +30,23 @@ class DefaultController extends AbstractController
         return $this->render('default/index.html.twig', [
           'users' => $users,
         ]);
+    }
+
+    /**
+      * @Route("/user/{id}", name="user_info", methods={"GET"})
+      */
+    public function user(int $id)
+    {
+      $user = $this->getDoctrine()
+        ->getRepository(Rankme::class)
+        ->find($id);
+
+      if (!$user) {
+        throw new NotFoundHttpException("User not found");
+      }
+
+      return $this->render('default/user.html.twig', [
+        'user' => $user,
+      ]);
     }
 }
